ac7a257fdf8d1a28357d1089c22a2c849271eb1e
Scandix <78552236+ScandixGaming@users.noreply.github.com>
2022-02-11 22:52:11 +0100

    Add files via upload

---
+++ esx_multicharacter-master/html/locales/en.js
@@ -0,0 +1,8 @@
+const translate = new Object();
+
+translate.name = "Navn";
+translate.job = "Job";
+translate.bank = "Bank";
+translate.money = "Kontant";
+translate.gender = "Køn";
+translate.dob = "Fødselsdag";
